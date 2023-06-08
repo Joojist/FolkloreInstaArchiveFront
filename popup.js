e2cd345488b5a1faf7b88b62779d91ba3ac17b65
Evelinotsa <135664496+Evelinotsa@users.noreply.github.com>
2023-06-08 13:27:09 +0300

    Sisi
    yesyes

---
+++ popup.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("urlForm");
+    var urlInput = document.getElementById("urlInput");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      var url = urlInput.value;
+      chrome.runtime.sendMessage({ action: "openTab", url: url }, function(response) {
+        alert(response.message);
+      });
+      urlInput.value = "";
+    });
+  });
