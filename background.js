e057c508351ed4e47d70b2bd46f6ba61d49ea61d
evelinotsa <evelin4@tlu.ee>
2023-06-09 10:47:34 +0300

    j

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-chrome.action.onClicked.addListener(tab => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.create({ url: "popup.html" });
-  });
-})
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.create({url: "popup.html"});
+});
