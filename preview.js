7a246decefda9db832b99396e2ad34cf4b93a8d6
Steve <zteven@tlu.ee>
2023-06-14 11:36:43 +0300

    table styles

--- preview.js
+++ preview.js
@@ -5,15 +5,17 @@
 // previewImage.src = imageUrl;
 
 
-// JavaScript code for the Save button functionality
-const saveButton = document.getElementById('saveButton');
-saveButton.addEventListener('click', () => {
-  // Get the input value from the URL input field
-  const urlInput = document.getElementById('urlInput');
-  const url = urlInput.value;
+window.addEventListener("DOMContentLoaded", function () {
+  // JavaScript code for the Save button functionality
+  const saveButton = document.getElementById('saveButton');
+  saveButton.addEventListener('click', () => {
+    // Get the input value from the URL input field
+    const urlInput = document.getElementById('urlInput');
+    const url = urlInput.value;
 
-  // Save the URL or perform any desired actions here
+    // Save the URL or perform any desired actions here
 
-  // Redirect to the archive preview page with the URL as a parameter
-  window.location.href = `preview.html?url=${encodeURIComponent(url)}`;
+    // Redirect to the archive preview page with the URL as a parameter
+    window.location.href = `preview.html?url=${encodeURIComponent(url)}`;
+  });
 });
