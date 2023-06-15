a958f6ed0f8aa11f466260f9ccc531f5af828f16
Steve <zteven@tlu.ee>
2023-06-15 15:27:24 +0300

    fetching image working + ui changes

--- post-saver.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("saveButton").addEventListener("click", submitForm);
-
-function submitForm() {
-    var form = document.getElementById("urlForm");
-
-    // Perform any necessary form validation or processing here
-
-    form.submit();
-}
