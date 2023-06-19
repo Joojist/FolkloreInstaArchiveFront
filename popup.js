5dabcc44580cf7c576ef0e8bd5e827c3a7969913
Steve <zteven@tlu.ee>
2023-06-19 12:14:49 +0300

    get more data about post

--- popup.js
+++ popup.js
@@ -1,6 +1,13 @@
 function getHtml() { return document.documentElement.innerHTML; }
+function parseDoc(html) {
+  const parser = new DOMParser();
+  return parser.parseFromString(html[0].result, "text/html");
+}
 function getImage(doc) { return doc.querySelector("div._aagv img").src; }
 function getTitle(doc) { return doc.querySelector("h1").innerHTML; }
+function getAuthor(doc) { return doc.querySelector("div._aaqt a.x1i10hfl").innerHTML; }
+function getAuthorAccountUrl(doc) { return doc.querySelector("div._aaqt a.x1i10hfl").href; }
+function getLikesAmount(doc) { return doc.querySelector("section._ae5m div div span a span span").innerHTML; }
 
 window.addEventListener("DOMContentLoaded", () => {
 
@@ -17,19 +24,14 @@ window.addEventListener("DOMContentLoaded", () => {
         func: getHtml,
       })
       .then(async (html) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html[0].result, "text/html");
-        
-        //const imgUrlsString = doc.querySelector("div._aagv img").srcset;
-        //const imgUrlsArray = imgUrlsString.split(",");
-        //const lastImgUrl = imgUrlsArray[imgUrlsArray.length - 1].trim();
-        //const imgUrl = lastImgUrl.substring(0, lastImgUrl.length - 6);
-
-        //const imgUrl = doc.querySelector("div._aagv img").src;
+        const doc = parseDoc(html);
       
         chrome.runtime.sendMessage({ action: "create-preview", data: { 
           imgUrl: getImage(doc),
-          title: getTitle(doc)
+          title: getTitle(doc),
+          author: getAuthor(doc),
+          authorAccountUrl: getAuthorAccountUrl(doc),
+          likes: parseInt(getLikesAmount(doc).replaceAll(",", ""))
         } });
       });
     });
